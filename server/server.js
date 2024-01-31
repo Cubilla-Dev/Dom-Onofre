f0556df8bd68a552514611bfddde16417a9c860f
Cubilla <cubilladev@gmail.com>
2024-01-30 23:04:14 -0300

    Se agrego Swagger para la documentacion de la api

--- server/server.js
+++ server/server.js
@@ -4,10 +4,16 @@ const config = require('./config')
 require('./mongoose.config')
 const morgan = require('morgan')
 
+const { swaggerDocs } = require('./configSwagger')
+
 const router = require('./src/routers/sesion.router')
+
 
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(router)
 
-app.listen(config.api.port, () => console.log(`Server en linea en el port: ${config.api.port}`))
+app.listen(config.api.port, () => {
+    console.log(`Server en linea en el port: ${config.api.port}`)
+    swaggerDocs(app, config.api.port)
+})
