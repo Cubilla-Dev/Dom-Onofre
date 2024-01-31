7703d47f90a3e6e472aaabf70db2939ab1159f24
Cubilla <cubilladev@gmail.com>
2024-01-31 09:00:11 -0300

    Se agreggo un Logger - falta de reparacion para que acepte los errores de la terminal

--- server/server.js
+++ server/server.js
@@ -3,17 +3,24 @@ const app = express()
 const config = require('./config')
 require('./mongoose.config')
 const morgan = require('morgan')
-
-const { swaggerDocs } = require('./configSwagger')
-
 const router = require('./src/routers/sesion.router')
 
+const { swaggerDocs } = require('./configSwagger')
+const buildLogger = require('./logger')
+
+//TODO: verificar mejor si esto esta bien 
+//para guardar los logs
+const logger = buildLogger('app.js');
+logger.log('Hola mundo')
+logger.error('Esto es algo malo')
 
 app.use(express.json())
+//para ver las peticiones que llega al server
 app.use(morgan('dev'))
 app.use(router)
 
 app.listen(config.api.port, () => {
     console.log(`Server en linea en el port: ${config.api.port}`)
+    //para la documentacion de los enpoints
     swaggerDocs(app, config.api.port)
 })
