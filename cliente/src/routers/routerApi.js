d4588e0c61c74c7f27777f355179125c8b14bfd4
Cubilla-Dev <137528287+Cubilla-Dev@users.noreply.github.com>
2024-02-21 08:08:35 -0300

    Add: la url del api

--- cliente/src/routers/routerApi.js
+++ cliente/src/routers/routerApi.js
@@ -1,5 +1,5 @@
 
-const PATH_API = 'http://3.136.154.133:3000'
+const PATH_API = 'https://dom-onofre-server-2.vercel.app/'
 export const PATH_API_ALL_PRODUCT = PATH_API + '/allproduct'
 export const PATH_API_SHOW_SALE = PATH_API + '/showsale/'
 export const PATH_API_CREATE_DEBT = PATH_API + '/create-debt'
